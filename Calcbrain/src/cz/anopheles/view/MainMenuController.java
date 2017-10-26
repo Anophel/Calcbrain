b16fc9bb8cbc0f8813f9969fc39fa89f76f0dc8e
Prtrik <Prtrik@147.32.93.196>
2017-10-26 22:04:02 +0200

    Initial commit.

---
+++ Calcbrain/src/cz/anopheles/view/MainMenuController.java
@@ -0,0 +1,24 @@
+package cz.anopheles.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class MainMenuController implements Initializable{
+
+	@FXML
+	private Text version;
+	
+	@FXML
+	private Button matrixOperations;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
